4c60f779dd6c8991c0b63ae4a4d53d6732e34422
Claudio Castro <claudioluiz.castro@gmail.com>
2019-01-08 17:45:59 -0200

    create first component

---
+++ src/components/Buscador.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="buscador">
+    <label for="buscador">Digite o termo que deseja buscar </label>
+    <input type="text" name="buscador" v-model="termo">
+    <p>{{termo}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Buscador',
+  data() {
+    return {
+      termo: '',
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
