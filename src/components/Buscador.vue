ab92c2c4e791b0220267297fcb78275d019ab237
Claudio Castro <claudioluiz.castro@gmail.com>
2019-01-31 09:35:59 -0200

    Add base.js and fuse.js

--- src/components/Buscador.vue
+++ src/components/Buscador.vue
@@ -2,17 +2,42 @@
   <div class="buscador">
     <label for="buscador">Digite o termo que deseja buscar </label>
     <input type="text" name="buscador" v-model="termo">
-    <p>{{termo}}</p>
+    <div class="resultado" v-if="listaAvioesEncontrados">
+      <ul>
+        <li v-for="aviao in listaAvioesEncontrados">
+          <b>{{aviao.aviao_nome}}</b>
+          <a :href="aviao.aviao_url">Veja mais sobre essa aeronave</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Data from '../base.js';
+import Fuse from 'fuse.js';
 export default {
   name: 'Buscador',
   data() {
     return {
       termo: '',
     }
+  },
+  computed: {
+    listaAvioesEncontrados() {
+      const fuseConfig = {
+        threshold: 0.5,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+          'aviao_nome',
+        ],
+      };
+      const fuse = new Fuse(Data, fuseConfig);
+      return fuse.search(this.termo);
+    },
   },
 }
 </script>
